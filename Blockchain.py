c348f31424941ee70aa72c5d4b1394b499ae0814
bennyp <benpang@outlook.com>
2017-11-24 00:42:06 -0500

    added functionality to hash() and new_block()

--- Blockchain.py
+++ Blockchain.py
@@ -1,13 +1,37 @@
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
 
 	def __init__(self):
 		self.chain = []
 		self.current_transactions = []
 
-	def new_block(self):
-		#create a new block and add to the chain
+	def new_block(self, proof, previous_hash=None):
+		"""
+		Create a new block and append it to the Blockchain
 
-		pass
+		:param proof: <int> The proof given by the Proof of Work algorithm
+		:param previous_hash: (Optional) <str> Hash of previous block
+		:return: <dict> New block
+		"""
+
+		block = {
+			'index': len(self.chain) + 1,
+			'timestamp': time(),
+			'transactions': self.current_transactions,
+			'proof': proof,
+			'previous_hash': previous_hash or self.hash(self.chain[-1]),
+		}
+		
+		#current transactions processed on new block, reset list of transactions
+		self.current_transactions = []
+
+		#append the new block to the chain
+		self.chain.append(block)
+
+		return block
 
 	def new_transaction(self, sender, recipient, amount):
 		"""
@@ -28,9 +52,16 @@ class Blockchain(object):
 
 	@staticmethod
 	def hash(block):
-		#Hashes a block
+		"""
+        Creates a SHA-256 hash of a Block
+        :param block: <dict> Block
+        :return: <str>
+        """
 
-		pass
+        #IMPORTANT: Block dict must be ordered, else possible inconsistencies
+		block_string = json.dumps(block, sort_keys=True).encode()
+		return hashlib.sha256(block_string).hexdigest()        
+
 
 	@property
 	def last_block(self):
