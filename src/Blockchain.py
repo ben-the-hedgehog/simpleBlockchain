a2412522688b9f94cf6ea32fe277ecfc1a213764
bennyp <benpang@outlook.com>
2018-01-10 23:10:17 -0500

    fixed bug in consensus

--- src/Blockchain.py
+++ src/Blockchain.py
@@ -1,116 +1,116 @@
 import hashlib
 import json
+import requests
+
 from textwrap import dedent
 from time import time
 from urllib.parse import urlparse
 
-#from flask import Flask, jsonify, request
-
 class Blockchain(object):
 
-	def __init__(self):
-		self.chain = []
-		self.current_transactions = []
-		self.nodes = set()
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+        self.nodes = set()
 
-		# Create the genesis block
-		self.new_block(previous_hash=1, proof=100)
+        # Create the genesis block
+        self.new_block(previous_hash=1, proof=100)
 
-	
-	def new_block(self, proof, previous_hash=None):
-		"""
-		Create a new block and append it to the Blockchain
+    
+    def new_block(self, proof, previous_hash=None):
+        """
+        Create a new block and append it to the Blockchain
 
-		:param proof: <int> The proof given by the Proof of Work algorithm
-		:param previous_hash: (Optional) <str> Hash of previous block
-		:return: <dict> New block
-		"""
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous block
+        :return: <dict> New block
+        """
 
-		block = {
-			'index': len(self.chain) + 1,
-			'timestamp': time(),
-			'transactions': self.current_transactions,
-			'proof': proof,
-			'previous_hash': previous_hash or self.hash(self.chain[-1]),
-		}
-		
-		#current transactions processed on new block, reset list of transactions
-		self.current_transactions = []
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+        
+        #current transactions processed on new block, reset list of transactions
+        self.current_transactions = []
 
-		#append the new block to the chain
-		self.chain.append(block)
+        #append the new block to the chain
+        self.chain.append(block)
 
-		return block
+        return block
 
-	def new_transaction(self, sender, recipient, amount):
-		"""
-		Creates a new transaction to go into the next mined Block
-		:param sender: <str> Address of the Sender
-		:param recipient: <str> Address of the Recipient
-		:param amount: <int> Amount
-		:return: <int> The index of the Block that will hold this transaction
-		"""
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Creates a new transaction to go into the next mined Block
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the Block that will hold this transaction
+        """
 
-		self.current_transactions.append({
-			'sender' : sender,
-			'recipient' : recipient,
-			'amount' : amount,
-			})
-		
-		return self.last_block['index'] + 1
+        self.current_transactions.append({
+            'sender' : sender,
+            'recipient' : recipient,
+            'amount' : amount,
+            })
+        
+        return self.last_block['index'] + 1
 
-	@staticmethod
-	def hash(block):
-		"""
-		Creates a SHA-256 hash of a Block
-		:param block: <dict> Block
-		:return: <str>
-		"""
+    @staticmethod
+    def hash(block):
+        """
+        Creates a SHA-256 hash of a Block
+        :param block: <dict> Block
+        :return: <str>
+        """
 
-		#IMPORTANT: Block dict must be ordered, else possible inconsistencies
-		block_string = json.dumps(block, sort_keys=True).encode()
-		return hashlib.sha256(block_string).hexdigest()        
+        #IMPORTANT: Block dict must be ordered, else possible inconsistencies
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()        
 
 
-	@property
-	def last_block(self):
-		"""
-		getter for last block on the chain
-		"""
-		return self.chain[-1]
+    @property
+    def last_block(self):
+        """
+        getter for last block on the chain
+        """
+        return self.chain[-1]
 
-	@staticmethod
-	def valid_proof(last_proof, proof):
-		"""
-		Validates proof: does hash(last_proof, proof) begin with 4 zeros?
-		"""
-		guess = f'{last_proof}{proof}'.encode()
-		guess_hash = hashlib.sha256(guess).hexdigest()
-		return guess_hash[:4]=="0000"
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validates proof: does hash(last_proof, proof) begin with 4 zeros?
+        """
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4]=="0000"
 
-	def proof_of_work(self, last_proof):
-		"""
-		find a number p' such that hash(pp') begins with 4 zeros
-		"""
-		proof = 0
+    def proof_of_work(self, last_proof):
+        """
+        find a number p' such that hash(pp') begins with 4 zeros
+        """
+        proof = 0
 
-		while self.valid_proof(last_proof, proof) is False:
-			proof += 1
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
 
-		return proof
+        return proof
 
-	def register_node(self, address):
-		"""
-		Register a new node in the list of nodes
+    def register_node(self, address):
+        """
+        Register a new node in the list of nodes
 
-		:param address: <str> Address of node. Eg. 'http://192.168.0.5:5000'
+        :param address: <str> Address of node. Eg. 'http://192.168.0.5:5000'
         :return: None
-		"""
-		parsed_url = urlparse(address)
-		self.nodes.add(parsed_url.netloc)
+        """
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
 
 
-	def valid_chain(self, chain):
+    def valid_chain(self, chain):
         """
         Determine if a given blockchain is valid
         :param chain: <list> A blockchain
